db48361666eeab090e77b3d453d85e123704d821
abisugi <72082288+abisugi@users.noreply.github.com>
2026-04-14 15:09:26 +0530

    Refactor conditionals for 'weird' check in Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,14 @@
+n = int(input().strip())
+
+if not n % 2 == 0:
+    print("weird")
+elif n%2 ==0 :
+    if  n>= 2 and n <=5 :
+        print("Not weird")
+
+    elif  n>=6 and n<=20:
+        print("weird")
+
+    elif  n > 20:
+        print("Not weird")
 
